0d234be8b81876f445937f3c4d7db8d2972359cc
Mr. Singleton <terrell.singleton@gmail.com>
2019-08-12 20:47:58 -0700

    added section component  and example

---
+++ starter-kit/src/app/shared/section/section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.scss']
+})
+export class SectionComponent implements OnInit {
+  @Input() title: string;
+  @Input() backgroundColor: string = 'white';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
--- starter-kit/src/app/shared/shared.module.ts
+++ starter-kit/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LoaderComponent } from './loader/loader.component';
 import { ProvidedServiceComponent } from './provided-service/provided-service.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [LoaderComponent, ProvidedServiceComponent],
-  exports: [LoaderComponent, ProvidedServiceComponent]
+  declarations: [LoaderComponent, ProvidedServiceComponent, SectionComponent],
+  exports: [LoaderComponent, ProvidedServiceComponent, SectionComponent]
 })
 export class SharedModule {}
